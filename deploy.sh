ed8e1fae12de77383ce8f404122ca3483fe3cf31
Will Ellis <william.elli5@icloud.com>
2025-11-06 15:37:44 +0000

    first commit

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Variables
+KEY_PATH=~/.ssh/will_cloud_deployment.pem
+EC2_HOST=ec2-user@3.10.175.99
+REMOTE_DIR=/home/ec2-user/message-server
+
+# Copy necessary files
+echo "> Uploading files to EC2"
+scp -i $KEY_PATH app.py requirements.txt Dockerfile $EC2_HOST:$REMOTE_DIR
+
+# Connect and run container
+ssh -i $KEY_PATH $EC2_HOST << EOF
+  cd message-server
+  docker build -t message-server .
+  docker stop message-server || true
+  docker rm message-server || true
+  docker run -p 5002:5002 \
+    -e APP_ENV=PRODUCTION \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_PASSWORD=MaKeRs1234 \
+    -e POSTGRES_HOST=will-message-server-db.cvruukypsgyb.eu-west-2.rds.amazonaws.com \
+    -e POSTGRES_DB=postgres \
+    message-server
+EOF
+
+echo "✅ Deployment complete! Visit: http://<your-ec2-ip>:5002"
