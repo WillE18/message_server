1f9944d46228038b4cd3fbddb768053f9d268176
Will Ellis <william.elli5@icloud.com>
2025-11-07 10:52:11 +0000

    deliberate test fail

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,6 @@ def test_get_home(web_client):
     assert response.status_code == 200
     assert "<h2>Your Messages</h2>" in response.data.decode("utf-8")
     assert "<form action=" in response.data.decode("utf-8")
+
+def test_deliberate_fail_to_see_workflow():
+    assert True == False
